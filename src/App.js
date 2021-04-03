0df24007de74e38e95c3332813362e132bae8807
David <dmatimu@udel.edu>
2021-04-03 16:28:02 -0400

    Country Dropdown is operational for all countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import {MenuItem, FormControl, Select,} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+//STATE = How to write a variable in REACT
+    //
+    useEffect(() => {
+        const getCountriesData = async () => {
+            await fetch("https://disease.sh/v3/covid-19/countries")
+                .then((response) => response.json())
+                .then((data) => {
+                    const countries = data.map((country) => ({
+                        name: country.country,
+                        value: country.countryInfo.iso2
+                    }));
+                    setCountries(countries)
+                })
+        }
+        getCountriesData();
+    }, [countries]);
+
+    return (
+        <div className="app">
+            <div className="app__header">
+                <h1>COVID-19 TRACKER</h1>
+                <FormControl className="app_dropdown">
+                    <Select variant="outlined" value="abc">
+                        {
+                            countries.map((country) => (
+                                <MenuItem value={country.value}>{country.name}</MenuItem>
+                            ))}
+                    </Select>
+                </FormControl>
+            </div>
+        </div>
+    );
 }
 
 export default App;
